941bf596883087db8064aa50735711041bbb01be
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-06-24 11:53:51 -0600

    Update get_local_A function

--- 3D/mef_utilities/mef_process.hpp
+++ 3D/mef_utilities/mef_process.hpp
@@ -10,6 +10,24 @@ float calculate_local_jacobian(float x1, float y1, float z1, float x2, float y2,
     return (x2-x1)*(y3-y1)*(z4-z1) - (x2-x1)*(y4-y1)*(z3-z1) - (x3-x1)*(y2-y1)*(z4-z1) + (x3-x1)*(y4-y1)*(z2-z1) + (x4-x1)*(y2-y1)*(z3-z1) - (x4-x1)*(y3-y1)*(z2-z1);
 }
 
+void calculate_local_A(Matrix* A, float x1, float y1, float z1, float x2, float y2, float z2, float x3, float y3, float z3, float x4, float y4, float z4){
+    float term1 = (y3 - y1) * (z4 - z1) - (y4 - y1) * (z3-z1);  
+    float term2 = -(x3 - x1) * (z4 - z1) + (x4 - x1) * (z3 - z1);  
+    float term3 = (x3 - x1) * (y4 - y1) - (x4 - x1) * (y3 - y1);
+    float term4 = -(y2 - y1) * (z4 - z1) + (y4 - y1) * (z2-z1); 
+    float term5 = (x2 - x1) * (z4 - z1) - (x4 - x1) * (z2 - z1);   
+    float term6 = -(x2 - x1) * (y4 - y1) + (x4 - x1) * (y2 - y1);
+    float term7 = (y2 - y1) * (z3 - z1) - (y3 - y1) * (z2-z1);  
+    float term8 = -(x2 - x1) * (z3 - z1) + (x3 - x1) * (z2 - z1);  
+    float term9 = (x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1);
+
+    A->set(term1, 0, 0); A->set(term2, 0, 1); A->set(term3, 0, 2);
+    A->set(term4, 1, 0); A->set(term5, 1, 1); A->set(term6, 1, 2);
+    A->set(term7, 2, 0); A->set(term8, 2, 1); A->set(term9, 2, 2);
+
+    return;
+}
+
 void calculate_B(Matrix* B){
     B->set(-1,0,0);  B->set(1,0,1);  B->set(0,0,2);  B->set(0,0,3);
     B->set(-1,1,0);  B->set(0,1,1);  B->set(1,1,2);  B->set(0,1,3);
@@ -17,10 +35,10 @@ void calculate_B(Matrix* B){
 }
 
 // TODO: update function
-void calculate_local_A(Matrix* A, float x1, float y1, float x2, float y2, float x3, float y3){
-    A->set(y3-y1, 0, 0);   A->set(x1-x3, 0, 1);
-    A->set(y1-y2, 1, 0);   A->set(x2-x1, 1, 1);
-}
+// void calculate_local_A(Matrix* A, float x1, float y1, float x2, float y2, float x3, float y3){
+//     A->set(y3-y1, 0, 0);   A->set(x1-x3, 0, 1);
+//     A->set(y1-y2, 1, 0);   A->set(x2-x1, 1, 1);
+// }
 
 // TODO: update function
 void create_local_K(Matrix* K, short element_id, Mesh* M){
