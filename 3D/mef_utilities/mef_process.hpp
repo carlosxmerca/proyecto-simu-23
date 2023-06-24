3b38f085fd2fac21fc270b4d657bb857a56be2a8
carlosxmerca <eduarmercado4@gmail.com>
2023-06-24 00:14:53 -0600

    Add calculate_local_volume func :sparkles:

--- 3D/mef_utilities/mef_process.hpp
+++ 3D/mef_utilities/mef_process.hpp
@@ -2,6 +2,10 @@ float calculate_local_area(float x1, float y1, float x2, float y2, float x3, flo
     return abs((x1*y2 + x2*y3 + x3*y1) - (x1*y3 + x2*y1 + x3*y2))/2;
 }
 
+float calculate_local_volume(float x1, float y1, float z1, float x2, float y2, float z2, float x3, float y3, float z3, float x4, float y4, float z4){
+    return abs( (x2-x1)*(y3-y1)*(z4-z1) + (y2-y1)*(z3-z1)*(x4-x1) + (x3-x1)*(y4-y1)*(z2-z1) - (z2-z1)*(y3-y1)*(x4-x1) - (y2-y1)*(x3-x1)*(z4-z1) - (z3-z1)*(y4-y1)*(x2-x1) )/6;
+}
+
 float calculate_local_jacobian(float x1, float y1, float z1, float x2, float y2, float z2, float x3, float y3, float z3, float x4, float y4, float z4){
     return (x2-x1)*(y3-y1)*(z4-z1) - (x2-x1)*(y4-y1)*(z3-z1) - (x3-x1)*(y2-y1)*(z4-z1) + (x3-x1)*(y4-y1)*(z2-z1) + (x4-x1)*(y2-y1)*(z3-z1) - (x4-x1)*(y3-y1)*(z2-z1);
 }
