e8635dd3100b6b4892340ee9664f32b044922504
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-06-30 23:38:11 -0600

    Add comments to source code and GiD file for a XL mesh :bulb::sparkles:

--- Heat transfer/3D/geometry/condition.hpp
+++ Heat transfer/3D/geometry/condition.hpp
@@ -1,25 +1,28 @@
 class Condition {
-    private:
-        Node* node;
-        float value;
+private:
+    Node* node;     // Pointer to the associated node
+    float value;    // Value of the condition
 
-    public:
-        Condition(Node* node_to_assign, float value_to_assign){
-            node = node_to_assign;
-            value = value_to_assign;
-        }
+public:
+    // Constructor
+    Condition(Node* node_to_assign, float value_to_assign) {
+        node = node_to_assign;
+        value = value_to_assign;
+    }
 
-        void set_node(Node* node_to_assign){
-            node = node_to_assign;
-        }
-        Node* get_node(){
-            return node;
-        }
+    // Setter and getter for node
+    void set_node(Node* node_to_assign) {
+        node = node_to_assign;
+    }
+    Node* get_node() {
+        return node;
+    }
 
-        void set_value(float value_to_assign){
-            value = value_to_assign;
-        }
-        float get_value(){
-            return value;
-        }
+    // Setter and getter for value
+    void set_value(float value_to_assign) {
+        value = value_to_assign;
+    }
+    float get_value() {
+        return value;
+    }
 };
--- Heat transfer/3D/geometry/element.hpp
+++ Heat transfer/3D/geometry/element.hpp
@@ -1,52 +1,58 @@
 class Element {
-    private:
-        int ID;
-        Node* node1;
-        Node* node2;
-        Node* node3;
-        Node* node4;
+private:
+    int ID;             // ID of the element
+    Node* node1;        // Pointer to the first node
+    Node* node2;        // Pointer to the second node
+    Node* node3;        // Pointer to the third node
+    Node* node4;        // Pointer to the fourth node
 
-    public:
-        Element (int identifier, Node* first_node, Node* second_node, Node* third_node, Node* fourth_node){
-            ID = identifier;
-            node1 = first_node;
-            node2 = second_node;
-            node3 = third_node;
-            node4 = fourth_node;
-        }
+public:
+    // Constructor
+    Element(int identifier, Node* first_node, Node* second_node, Node* third_node, Node* fourth_node) {
+        ID = identifier;
+        node1 = first_node;
+        node2 = second_node;
+        node3 = third_node;
+        node4 = fourth_node;
+    }
 
-        void set_ID(int identifier){
-            ID = identifier;
-        }
-        int get_ID(){
-            return ID;
-        }
+    // Setter and getter for ID
+    void set_ID(int identifier) {
+        ID = identifier;
+    }
+    int get_ID() {
+        return ID;
+    }
 
-        void set_node1(Node* node){
-            node1 = node;
-        }
-        Node* get_node1(){
-            return node1;
-        }
+    // Setter and getter for node1
+    void set_node1(Node* node) {
+        node1 = node;
+    }
+    Node* get_node1() {
+        return node1;
+    }
 
-        void set_node2(Node* node){
-            node2 = node;
-        }
-        Node* get_node2(){
-            return node2;
-        }
+    // Setter and getter for node2
+    void set_node2(Node* node) {
+        node2 = node;
+    }
+    Node* get_node2() {
+        return node2;
+    }
 
-        void set_node3(Node* node){
-            node3 = node;
-        }
-        Node* get_node3(){
-            return node3;
-        }
+    // Setter and getter for node3
+    void set_node3(Node* node) {
+        node3 = node;
+    }
+    Node* get_node3() {
+        return node3;
+    }
 
-        void set_node4(Node* node){
-            node4 = node;
-        }
-        Node* get_node4(){
-            return node4;
-        }
+    // Setter and getter for node4
+    void set_node4(Node* node) {
+        node4 = node;
+    }
+    Node* get_node4() {
+        return node4;
+    }
 };
